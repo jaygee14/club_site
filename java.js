c9db257260d4fbdb8468c227325c93031b0e17c7
jaygee14 <Jared3257@gmail.com>
2023-08-29 15:41:56 -0400

    Add forms and Java file

---
+++ java.js
@@ -0,0 +1,10 @@
+sign = document.getElementsByClassName('signUp')
+tab = document.getElementsByClassName('sign-up-sheet')
+
+function showInfo() {
+    tab.style.display = 'none';
+};
+
+sign.onclick = showInfo;
+
+
